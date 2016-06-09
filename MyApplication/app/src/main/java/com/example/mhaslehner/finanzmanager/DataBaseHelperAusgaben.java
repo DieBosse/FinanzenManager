1a4ade36534d9734b7fa622169e20bffc74760c0
maxbruce <bruce-lang@hotmail.com>
2016-06-09 13:53:19 +0200

    DataBaseHelper fertiggestellt /max
    Helper für Datenbanken Ein und Ausnahmen fertiggestellt.

--- MyApplication/app/src/main/java/com/example/mhaslehner/finanzmanager/DataBaseHelperAusgaben.java
+++ MyApplication/app/src/main/java/com/example/mhaslehner/finanzmanager/DataBaseHelperAusgaben.java
@@ -1,25 +1,28 @@
 package com.example.mhaslehner.finanzmanager;
 
+import android.content.ContentValues;
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
 
 /**
  * Created by mhaslehner on 09.06.2016.
  */
-public class DataBaseHelperAusgaben {
+public class DataBaseHelperAusgaben extends SQLiteOpenHelper{
 
-    public DataBaseOpenHelperAusgaben(Context context) {
-        super(context, "name", null, 1);
+
+    public DataBaseHelperAusgaben(Context context) {
+        super(context, Constants.DBNAME_A, null, Constants.DBVERSION_A);
     }
 
     @Override
     public void onCreate(SQLiteDatabase db) {
-        db.execSQL(TBLSQL);
+        db.execSQL(Constants.TBLSQL_A);
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
-        db.execSQL("DROP TABLE DETAILS");
+        db.execSQL("DROP TABLE "+Constants.TBLNAME_A);
         onCreate(db);
     }
 }
--- MyApplication/app/src/main/java/com/example/mhaslehner/finanzmanager/DataBaseHelperEinnahmen.java
+++ MyApplication/app/src/main/java/com/example/mhaslehner/finanzmanager/DataBaseHelperEinnahmen.java
@@ -2,25 +2,26 @@ package com.example.mhaslehner.finanzmanager;
 
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
 
 /**
  * Created by mhaslehner on 09.06.2016.
  */
-public class DataBaseHelperEinnahmen {
+public class DataBaseHelperEinnahmen extends SQLiteOpenHelper{
 
 
-    public DataBaseOpenHelper(Context context) {
-        super(context, DBNAME_E, null, 11);
+    public DataBaseHelperEinnahmen(Context context) {
+        super(context, Constants.DBNAME_E, null, Constants.DBVERSION_E);
     }
 
     @Override
     public void onCreate(SQLiteDatabase db) {
-        db.execSQL(TBLSQL);
+        db.execSQL(Constants.TBLSQL_E);
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
-        db.execSQL("DROP TABLE DETAILS");
+        db.execSQL("DROP TABLE "+Constants.TBLNAME_E);
         onCreate(db);
     }
 }
