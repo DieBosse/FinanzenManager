25ff04a9d88d3dd5172a897940e073bb2e384fa8
maxbruce <bruce-lang@hotmail.com>
2016-06-10 13:05:58 +0200

    Kategorien hinzufügen/blang
    Fertig gestellt.

--- MyApplication/app/src/main/java/com/example/mhaslehner/finanzmanager/KategorieVerwaltung.java
+++ MyApplication/app/src/main/java/com/example/mhaslehner/finanzmanager/KategorieVerwaltung.java
@@ -1,11 +1,17 @@
 package com.example.mhaslehner.finanzmanager;
 
+import android.app.Dialog;
+import android.content.ContentValues;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.support.v4.widget.SimpleCursorAdapter;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.Toast;
 
 /**
  * Created by HP on 09.06.2016.
@@ -14,6 +20,7 @@ public class KategorieVerwaltung extends AppCompatActivity {
     private static DataBaseHelperKategorien dataBaseHelperKategorien;
     private static ListView listkategorien = null;
     private static SQLiteDatabase db = null;
+    Button addKategorieButton = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,6 +30,13 @@ public class KategorieVerwaltung extends AppCompatActivity {
         listkategorien = (ListView) findViewById(R.id.listViewKategorien);
         dataBaseHelperKategorien = new DataBaseHelperKategorien(this);
         db = dataBaseHelperKategorien.getWritableDatabase();
+        addKategorieButton = (Button) findViewById(R.id.buttonAddKategorie);
+        addKategorieButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                addKategorie();
+            }
+        });
         showKategories();
     }
 
@@ -30,20 +44,54 @@ public class KategorieVerwaltung extends AppCompatActivity {
         Cursor cursor = db.rawQuery("SELECT * FROM " + Constants.TBLNAME_K, null);
         if (cursor.moveToFirst()) {
             setAdapter(cursor);
+
         }
     }
 
     private void setAdapter(Cursor cursor) {
         final String[] COLUMS_TO_BE_BOUND = new String[]{Constants.KATEGORIENAME};
         final int[] ITEMS_TO_BE_FILLED = new int[]{android.R.id.text1};
-        final SimpleCursorAdapter adapter = new SimpleCursorAdapter(this, android.R.layout.simple_list_item_1,
+        final SimpleCursorAdapter adapter = new SimpleCursorAdapter(this,
+                android.R.layout.simple_list_item_1,
                 cursor,
                 COLUMS_TO_BE_BOUND,
                 ITEMS_TO_BE_FILLED, 0);
         listkategorien.setAdapter(adapter);
     }
 
-    public static void addKategorie() {
+    public void addKategorie() {
+        final Dialog dialog = new Dialog(this);
+        View v = getLayoutInflater().inflate(R.layout.dialog_add_kategorie, null);
+        dialog.setContentView(v);
+        final EditText nameeditText = (EditText) v.findViewById(R.id.editTextAddKategorieName);
+        final EditText beschreibungeditText = (EditText) v.findViewById(R.id.editTextAddKategorieBeschreibung);
+        Button addbutoon = (Button) v.findViewById(R.id.buttonAddKategorie);
+        Button cancelbutton = (Button) v.findViewById(R.id.buttonCancelKategorie);
+        dialog.show();
+        cancelbutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dialog.cancel();
+            }
+        });
+
+        addbutoon.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ContentValues vals = new ContentValues();
+                String name = nameeditText.getText().toString();
+                String beschreibung = beschreibungeditText.getText().toString();
+                vals.put(Constants.KATEGORIENAME, name);
+                vals.put(Constants.BESCHREIBUNG, beschreibung);
+                long insertid = db.insert(Constants.TBLNAME_K, null, vals);
+                dialog.cancel();
+                if (insertid > 0) {
+                    Toast.makeText(getApplicationContext(), "Die Kategorie " +
+                            name + " wurde erfolgreich erstellt.", Toast.LENGTH_LONG).show();
+                }
+            }
+        });
+        showKategories();
 
     }
 
